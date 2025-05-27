d6a53a90226dc992984525967879ae9353f396df
nguyenthanhanton <ton01022004@gmail.com>
2025-05-27 10:21:32 +0700

    them form dang nhap

---
+++ codeC#/Quan_Ly_Lich_Thuc_Hanh_Phong_May/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_Ly_Lich_Thuc_Hanh_Phong_May
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txt_pass.UseSystemPasswordChar = false; // Hiện mật khẩu
+            }
+            else
+            {
+                txt_pass.UseSystemPasswordChar = true;  // Ẩn mật khẩu
+            }
+
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc muốn thoát không?","Thông báo",MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button2);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit(); // hoặc this.Close();
+            }
+
+        }
+    }
+}
